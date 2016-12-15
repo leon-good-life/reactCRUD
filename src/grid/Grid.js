59b4616e2aaa7a5ce6972d4854b073be97079a76
leon-good-life <leon.good.life@gmail.com>
2016-12-15 14:40:59 +0200

    Added reusable Grid component

---
+++ src/grid/Grid.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <table>
+        <tr>
+          {Object.keys(this.props.data[0]).map(columnName=>(
+              <th>{columnName}</th>
+          ))}
+        </tr>
+        {this.props.data.map(rowObj=>(
+          <tr>
+            {Object.keys(rowObj).map(item=>(
+              <td>{rowObj[item]}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
+    );
+  }
+}
+
+export default Grid;
