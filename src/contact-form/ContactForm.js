f8b4e817682d984a64bb1dde62217cd2beb81c05
leon-good-life <leon.good.life@gmail.com>
2016-12-17 19:00:10 +0200

    Added create-new-contact feature

--- src/contact-form/ContactForm.js
+++ src/contact-form/ContactForm.js
@@ -6,22 +6,41 @@ class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.handleCancel = this.handleCancel.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    if (this.props.data) {
+      this.data = this.props.data;
+    } else {
+      this.data = {
+        id: String(Date.now()),
+        userName: '',
+        lastName: '',
+        company: '',
+        phone: '',
+        email: ''
+      }
+    }
   }
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <input type="hidden"
-               value={this.props.data.id} />
-        <input value={this.props.data.firstName}
-               placeholder="First Name" />
-        <input value={this.props.data.lastName}
-               placeholder="Last Name" />
-        <input value={this.props.data.company}
-               placeholder="Company" />
-        <input value={this.props.data.phone}
-               placeholder="Phone" />
-        <input value={this.props.data.email}
-               placeholder="Email" />
+               value={this.data.id}
+               name="id" />
+        <input defaultValue={this.data.firstName}
+               placeholder="First Name"
+               name="firstName" />
+        <input defaultValue={this.data.lastName}
+               placeholder="Last Name"
+               name="lastName" />
+        <input defaultValue={this.data.company}
+               placeholder="Company"
+               name="company" />
+        <input defaultValue={this.data.phone}
+               placeholder="Phone"
+               name="phone" />
+        <input defaultValue={this.data.email}
+               placeholder="Email"
+               name="email" />
         <button type="submit">Save</button>
         <button type="button"
                 onClick={this.handleCancel}>Cancel</button>
@@ -30,6 +49,17 @@ class ContactForm extends Component {
   }
   handleCancel() {
     browserHistory.push('contacts-list');
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleSubmit({
+      id: e.target.querySelector('[name="id"]').value,
+      firstName: e.target.querySelector('[name="firstName"]').value,
+      lastName: e.target.querySelector('[name="lastName"]').value,
+      company: e.target.querySelector('[name="company"]').value,
+      phone: e.target.querySelector('[name="phone"]').value,
+      email: e.target.querySelector('[name="email"]').value
+    });
   }
 }
 
--- src/contact-form/CreateNewContact.js
+++ src/contact-form/CreateNewContact.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 import ContactForm from './ContactForm';
+import { addContact } from '../redux/actions';
+import { connect } from 'react-redux';
 
 class CreateNewContact extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.data = {
-      id:'',
-      userName: '',
-      lastName: '',
-      company: '',
-      phone: '',
-      email: ''
-    }
   }
   render() {
     return (
       <div>
         <h1>Create New Contact</h1>
-        <ContactForm handleSubmit={this.handleSubmit}
-                     data={this.data} />
+        <ContactForm handleSubmit={this.handleSubmit} />
       </div>
     );
   }
-  handleSubmit() {
-
+  handleSubmit(contactObj) {
+    this.props.dispatch(addContact(contactObj));
+    browserHistory.push('contacts-list');
   }
 }
+
+CreateNewContact = connect()(CreateNewContact);
 
 export default CreateNewContact;
