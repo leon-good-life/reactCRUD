9321c90f427b687410fd622f9a23c490661a1a44
leon-good-life <leon.good.life@gmail.com>
2016-12-24 18:09:46 +0200

    fixed

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
 import TableHeaders from './TableHeaders'
-import { tableRows } from './tableRows'
+import tableRows from './tableRows'
 import './Grid.css'
 
 class Grid extends Component {
--- src/grid/TableHeaders.js
+++ src/grid/TableHeaders.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import _ from 'lodash'
 
 const whichClass = (column, state) => state.column === column ?
   state.direction : ''
 
-const TableHeaders = ({columns, state, setSortState, showActions}) =>
-  <tr>
-    columns.map((column, i)=>
-      <th key={'column'+i}
-        onClick={()=>setSortState(column, state)}
-        className={whichClass(column, state)}
-        >{_.startCase(column)}</th>)
+class TableHeaders extends Component {
+  render() {
+    const columns = this.props.columns
+    const state = this.props.state
+    const setSortState = this.props.setSortState
+    const showActions = this.props.showActions
+    return <tr>
+      {columns.map((column, i)=>
+        <th key={'column'+i}
+          onClick={()=>setSortState(column, state)}
+          className={whichClass(column, state)}
+          >{_.startCase(column)}</th>)}
 
-    {showActions ? <th>Actions</th> : null}
-  </tr>
+      {showActions ? <th>Actions</th> : null}
+    </tr>
+  }
+}
 
 export default TableHeaders
