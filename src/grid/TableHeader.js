21e1219fb087cfefda9024313a5c9896628a7855
leon-good-life <leon.good.life@gmail.com>
2016-12-24 21:15:34 +0200

    TableHeader.js

--- src/grid/TableHeader.js
+++ src/grid/TableHeader.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import _ from 'lodash'
 
 const whichClass = (column, state) => state.column === column ?
   state.direction : ''
 
-class TableHeaders extends Component {
-  render() {
-    const columns = this.props.columns
-    const state = this.props.state
-    const setSortState = this.props.setSortState
-    const showActions = this.props.showActions
-    return (
-      <thead>
-        <tr>
-          {columns.map((column, i)=>
-            <th key={'column'+i}
-                onClick={()=>setSortState(column, state)}
-                className={whichClass(column, state)}
-                >{_.startCase(column)}</th>)}
+const TableHeaders = ({columns, state, setSortState, showActions}) =>
+  <thead>
+    <tr>
+      {columns.map((column, i)=>
+        <th key={'column'+i}
+            onClick={()=>setSortState(column, state)}
+            className={whichClass(column, state)}
+            >{_.startCase(column)}</th>)}
 
-          {showActions ? <th>Actions</th> : null}
-        </tr>
-      </thead>
-    )
-  }
-}
+      {showActions ? <th>Actions</th> : null}
+    </tr>
+  </thead>
 
 export default TableHeaders
